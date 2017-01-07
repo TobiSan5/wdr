3969bcfe6624f3fe97aee34a5df226bc58c945d9
TobiSan5 <tw.engel@gmail.com>
2017-01-07 13:31:44 +0100

    Trouble all the way with unitconverter.js

--- docs/unitconverter.js
+++ docs/unitconverter.js
@@ -1,53 +1,32 @@
-function keyify(text_str){
-  text_str = text_str.toLowerCase()
-  text_str = text_str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "_");
-  return text_str;  
-};
-
-var unitArray = {
-  'm': {
-    'm2spa_kg': 1.95e-10
-   }
-};
 
 
-$(document).ready(function(){
-  $("#btn1").click(function() {
-    var val = +$("#value").val();
-    var i_unit = +$("#inp_unit").val();
-    var o_unit = +$("#out_unit").val();
-    var ikey = keyify(i_unit);
-    var okey = keyify(o_unit);
-    var res = val * unitArray[okey][ikey]
-    $("#result").val(res);
-    $("#res_unit").val(o_unit);
-  });
-});		
 
-/* No work..
-function funcCalc(jQuery, val, i_unit, o_unit)
-{
-  var value = document.getElementById('value').value;
-  var  inp_unit = document.getElementById('inp_unit').value; 
-  var out_unit = document.getElementById('out_unit').value;
-  var unitArray = {};
-  unitArray.m.m2sPa_kg = 1.95e-10
-  i_unit = i_unit.replace("/", "_");
-  o_unit = o_unit.replace("/", "_");
-  return unitArray[o_unit][i_unit] * 
-}
 
-$(document).ready(function(){
-    $(".calc_button").click(
-    function(){
-          var val = +$(".value").val();
-          var i_unit = +$(".inp_unit").val();
-          var o_unit = +$(".out_unit").val();
-          var res = funcCalc(val, i_unit, o_unit);
-          Alert(res);
-          $("#result").val(res);
-   });
-});
-*/
+$(document).ready(function(){$("#btn1").click(function() {
+  function keyify(text_str){
+    text_str = String(text_str).toLowerCase()
+    text_str = text_str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "_");
+    return text_str;  
+  };  
+  
+  var unitArray = {
+    'm': {
+      'm2spa_kg': 1.95e-10
+    }
+  };
+  
+  var val = +$("#value").val();
+  console.log(val);
+  var i_unit = +$("#inp_unit").val();
+  console.log(val);    
+  var o_unit = +$("#out_unit").val();
+  console.log(val);
+  var ikey = keyify(i_unit);
+  console.log(val);
+  var okey = keyify(o_unit);
+  console.log(val);
+  // var res = val * unitArray[okey][ikey]
+  $("#result").val(111);
+  $("#res_unit").val(o_unit);  
+});});		
 
-// test writing with Firefox Web IDE (ctrl+s saves!)
