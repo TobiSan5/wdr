6e4fe41407ed65a9486ca3092b894a237835f765
TobiSan5 <tw.engel@gmail.com>
2017-01-07 14:22:33 +0100

    Trying to get jQuery and javascript function to work for unit converter
    Finally.. Yes?

--- docs/unitconverter.js
+++ docs/unitconverter.js
@@ -1,32 +1,30 @@
-
-
-
-
-$(document).ready(function(){$("#btn1").click(function() {
-  function keyify(text_str){
-    text_str = String(text_str).toLowerCase()
-    text_str = text_str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "_");
-    return text_str;  
-  };  
+function keyify(text_str){
+	text_str = String(text_str).toLowerCase()
+	text_str = text_str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "_");
+	return text_str;  
+};  
   
-  var unitArray = {
-    'm': {
-      'm2spa_kg': 1.95e-10
-    }
-  };
-  
-  var val = +$("#value").val();
-  console.log(val);
-  var i_unit = +$("#inp_unit").val();
-  console.log(val);    
-  var o_unit = +$("#out_unit").val();
-  console.log(val);
-  var ikey = keyify(i_unit);
-  console.log(val);
-  var okey = keyify(o_unit);
-  console.log(val);
-  // var res = val * unitArray[okey][ikey]
-  $("#result").val(111);
-  $("#res_unit").val(o_unit);  
-});});		
+var unitArray = {
+	'm': {
+		'm2spa_kg': 1.95e-10
+		}
+	};
+
+function runConvert(val, i_unit, o_unit) {
+  var i_key = keyify(i_unit);
+  var o_key = keyify(o_unit);
+  var res = val * unitArray[o_key][i_key]
+  return res
+};
+
+$(document).ready(function(){
+	var p1 = $("#value").val()
+	var p2 = $("#inp_unit").val()
+	var p3 = $("#out_unit").val()
+	$("#btn1").click(function(){
+		var result = runConvert(p1, p2, p3); 
+	});
+	$("#result").val(result);
+	$("#res_unit").val(p3);  
+});
 
