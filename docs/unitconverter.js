faf67bcda23074e4804fce18e904185b32e78fa8
TobiSan5 <tw.engel@gmail.com>
2017-01-07 14:57:44 +0100

    Logic for unit convert shared between html doc and script file
    Final try for today..

--- docs/unitconverter.js
+++ docs/unitconverter.js
@@ -10,21 +10,4 @@ var unitArray = {
 		}
 	};
 
-function runConvert(val, i_unit, o_unit) {
-  var i_key = keyify(i_unit);
-  var o_key = keyify(o_unit);
-  var res = val * unitArray[o_key][i_key]
-  return res
-};
-
-$(document).ready(function(){
-	var p1 = $("#value").val()
-	var p2 = $("#inp_unit").val()
-	var p3 = $("#out_unit").val()
-	$("#btn1").click(function(){
-		var result = runConvert(p1, p2, p3); 
-	});
-	$("#result").val(result);
-	$("#res_unit").val(p3);  
-});
 
